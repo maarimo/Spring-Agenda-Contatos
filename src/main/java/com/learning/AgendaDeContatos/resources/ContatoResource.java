ca900dc30113b27e51e631fb6d6c8223ced156f7
Matheus Amorim <abmm1234@hotmail.com>
2025-10-23 16:15:05 -0300

    create restcontroll

--- src/main/java/com/learning/AgendaDeContatos/resources/ContatoResource.java
+++ src/main/java/com/learning/AgendaDeContatos/resources/ContatoResource.java
@@ -2,11 +2,11 @@ package com.learning.AgendaDeContatos.resources;
 
 import com.learning.AgendaDeContatos.entities.Contato;
 import com.learning.AgendaDeContatos.services.ContatoService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,5 +20,30 @@ public class ContatoResource {
     public ResponseEntity<List<Contato>> listar(){
         List<Contato> list = service.listar();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Contato> buscar(@PathVariable Long id){
+        Contato contato = service.buscarPorId(id);
+        return ResponseEntity.ok(contato);
+    }
+
+    @PostMapping
+    public ResponseEntity<Contato> criar(@Valid @RequestBody Contato contato){
+        Contato criado = service.criar(contato);
+        URI uri =URI.create("/contatos/" + criado.getId());
+        return ResponseEntity.created(uri).body(criado);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Contato> atualizar(@PathVariable Long id, @Valid @RequestBody Contato contato){
+        Contato atualizado = service.atualizar(id, contato);
+        return ResponseEntity.ok(atualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
     }
 }
